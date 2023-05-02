afa9a68e247392e6ebddc6ae807d4b1328bcde55
AiRISS <jinnam0326@gmail.com>
2023-05-03 08:48:07 +0900

    ⚰️ remove : dead code

--- models/build.py
+++ models/build.py
@@ -1,6 +1,6 @@
-from .dcgan import (generator64,generator128,generator256,generator512,
+from .dcgan import (generator64,generator128,generator256,
                     discriminator64,discriminator128,discriminator256)
-from .lsgan import lsdiscriminator64,lsdiscriminator128,lsdiscriminator256,lsdiscriminator512
+from .lsgan import lsdiscriminator64,lsdiscriminator128,lsdiscriminator256
 
 MODELS = {
     "dcgan64":[generator64,discriminator64],
@@ -9,14 +9,13 @@ MODELS = {
     "lsgan64":[generator64,lsdiscriminator64],
     "lsgan128":[generator128,lsdiscriminator128],
     "lsgan256":[generator256,lsdiscriminator256],
-    "lsgan512":[generator512,lsdiscriminator512],
 }
 
 def get_model(model:str,vector_size:int=100,ngf:int=64,ndf:int=64):
     models = MODELS.get(model,None)
     
     if models is None:
-        raise ModuleNotFoundError(f"해당하는 모델이 존재하지 않습니다. 모델 : {model}")
+        raise ModuleNotFoundError(f"Not found : {model}")
     
     generator = models[0](vector_size,ngf)
     discriminator = models[1](ndf)
